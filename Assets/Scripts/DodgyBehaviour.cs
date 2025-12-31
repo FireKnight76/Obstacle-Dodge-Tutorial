141087b6fd07118414f4c240f4a3c1ed34b13ba2
FireKnight76 <fireknight593@gmail.com>
2025-12-31 13:42:29 +0100

    Setup with moveable character

---
+++ Assets/Scripts/DodgyBehaviour.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class DodgyBehaviour : MonoBehaviour
+{
+    [SerializeField] float dodgySpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * dodgySpeed;
+        float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * dodgySpeed;
+
+        transform.Translate(moveX, 0, moveZ);
+    }
+}
